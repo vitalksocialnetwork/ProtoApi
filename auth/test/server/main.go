35d1a1b430b7f802307be2de7eb9d5b317db5a1c
m0lw9re <xuantungmta@gmail.com>
2021-10-11 21:31:31 +0700

    oke

---
+++ auth/test/server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"time"
+
+	pb "github.com/vitalksocialnetwork/ProtoApi/auth/public"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	pb.UnimplementedAuthServiceServer
+}
+
+func (s *server) Ping(ctx context.Context, in *pb.Ping) (*pb.Pong, error) {
+	log.Printf("Received: %v", in.GetTimestamp())
+	return &pb.Pong{Timestamp: time.Now().Unix()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterAuthServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
+}
