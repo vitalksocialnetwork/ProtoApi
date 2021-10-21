4bee28a22c147854062e66e323cfa7cda1af4f03
m0lw9re <xuantungmta@gmail.com>
2021-10-21 16:34:29 +0700

    oke

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/vitalksocialnetwork/ProtoApi
 go 1.17
 
 require (
+	google.golang.org/grpc v1.41.0
+	google.golang.org/protobuf v1.27.1
+)
+
+require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/net v0.0.0-20211008194852-3b03d305991f // indirect
 	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20211008145708-270636b82663 // indirect
-	google.golang.org/grpc v1.41.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
 )
---
+++ profile/test/client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/vitalksocialnetwork/ProtoApi/profile/public"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewProfileServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Ping(ctx, &pb.Ping{Timestamp: time.Now().Unix()})
+	if err != nil {
+		log.Fatalf("could not ping: %v", err)
+	}
+	log.Printf("Pong: %d", r.GetTimestamp())
+}
